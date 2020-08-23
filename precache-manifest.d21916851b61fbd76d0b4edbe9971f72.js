7ed066d4bc2ed9122f018a0c6932358d89a5aae8
Thomas <thomas-ln@laposte.net>
2020-08-23 16:59:09 +0200

    Updates

---
+++ precache-manifest.d21916851b61fbd76d0b4edbe9971f72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d0f6d62be63494f3366b2ba0c43562",
+    "url": "/react-canvas-draw/index.html"
+  },
+  {
+    "revision": "3a3382ee6ae3e824ac68",
+    "url": "/react-canvas-draw/static/css/main.f355d035.chunk.css"
+  },
+  {
+    "revision": "0df2718e277fcd8e1e3c",
+    "url": "/react-canvas-draw/static/js/2.0f88b929.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-canvas-draw/static/js/2.0f88b929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3382ee6ae3e824ac68",
+    "url": "/react-canvas-draw/static/js/main.9e439ace.chunk.js"
+  },
+  {
+    "revision": "8e48ee28409d63fbeb8a",
+    "url": "/react-canvas-draw/static/js/runtime-main.3d96b9fa.js"
+  }
+]);
